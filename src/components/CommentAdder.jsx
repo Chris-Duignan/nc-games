ba5b6c4d6e739fe1e30537ec62e0022c72abcbf4
Chris-Duignan <cduignan.1993@gmail.com>
2022-11-02 13:41:25 +0000

    add submit feature to comments box

--- src/components/CommentAdder.jsx
+++ src/components/CommentAdder.jsx
@@ -1,19 +1,91 @@
-const CommentAdder = ({ newComment, setNewComment }) => {
+import { useState } from "react";
+import { UserContext } from "./User";
+import { useContext, useEffect } from "react";
+import { postCommentsByReviewId } from "../api";
+import CommentsList from "./CommentsList";
+import { fetchCommentsByReviewId } from "../api";
 
-  const handleChange = (event)=> {
-    setNewComment({...newComment, [event.target.name] : event.target.value})
-  }
+const CommentAdder = ({ review_id }) => {
+  const [count, setCount] = useState(0);
+  const { user } = useContext(UserContext);
+  const [comment, setComment] = useState({
+    author: user,
+    body: "",
+  });
+  const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [err, setErr] = useState(null);
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setErr(null);
+    fetchCommentsByReviewId(review_id)
+      .then((comments) => {
+        setComments(comments);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setErr("Oops, something went wrong, try again later");
+      });
+  }, [review_id]);
+
+  const handleChange = (event) => {
+    setComment({
+      ...comment,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setCount((currCount) => currCount + 1);
+    setIsDisabled(true);
+    postCommentsByReviewId(review_id, comment.author, comment.body)
+      .then((res) => {
+        setComments((currComments) => [res.data.comment, ...currComments]);
+        setIsDisabled(false);
+      })
+      .catch((err) => {
+        setErr(err.response.data.msg);
+        setIsDisabled(false);
+      });
+    setComment({
+      comment_id: `Opt${count}`,
+      author: user,
+      body: "",
+      votes: 0,
+      created_at: "Now",
+    });
+  };
 
   return (
-    <form>
-      <input
-        className="messageInput"
-        placeholder="enter your comment"
-        value={newComment.body}
-        onChange={handleChange}
-        name="body"
-      ></input>
-    </form>
+    <section className="commentsContainer">
+      <form onSubmit={handleSubmit}>
+        {err ? <h3>{err}</h3> : null}
+        {isDisabled ? (
+          <h3>Please Wait</h3>
+        ) : (
+          <>
+            <label id="commentLabel" htmlFor="messageInput">
+              Post Comment
+            </label>
+            <input
+              className="messageInput"
+              placeholder="enter your comment"
+              value={comment.body}
+              onChange={handleChange}
+              name="body"
+              id="messageInput"
+            ></input>
+          </>
+        )}
+        <button type="submit" className="messageSubmit">
+          <b>Submit</b>
+        </button>
+      </form>
+      {isLoading ? <h3>Loading ...</h3> : <CommentsList comments={comments} />}
+    </section>
   );
 };
 
--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -1,45 +1,14 @@
-import { useEffect, useState, useContext } from "react";
-import { fetchCommentsByReviewId } from "../api";
-import { UserContext } from "./User";
-
 import Comment from "./Comment";
-import CommentAdder from "./CommentAdder";
 import CommentPlaceholder from "./CommentPlaceHolder";
 
-const CommentsList = ({ review_id }) => {
-  const {user} = useContext(UserContext);
-  const [newComment, setNewComment] = useState({ username: user, body: "" });
-  const [comments, setComments] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [err, setErr] = useState(null);
-
-  
-
-  useEffect(() => {
-    setIsLoading(true);
-    setErr(null);
-    fetchCommentsByReviewId(review_id)
-      .then((comments) => {
-        setComments(comments);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        setErr("Oops, something went wrong, try again later");
-      });
-  }, [review_id]);
-
-  if (err) return <h3>{err}</h3>;
-  if (isLoading) return <h3>Loading ...</h3>;
+const CommentsList = ({ comments }) => {
   return (
-    <section className="commentsContainer">
-      <CommentAdder newComment={newComment} setNewComment={setNewComment} />
-      <ul className="commentsList">
-        <CommentPlaceholder comments={comments} />
-        {comments.map((comment) => {
-          return <Comment key={comment.comment_id} comment={comment} />;
-        })}
-      </ul>
-    </section>
+    <ul className="commentsList">
+      <CommentPlaceholder comments={comments} />
+      {comments.map((comment) => {
+        return <Comment key={comment.comment_id} comment={comment} />;
+      })}
+    </ul>
   );
 };
 
