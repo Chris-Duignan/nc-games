f17dd28c3500f7fb70b276aba380d2bc8bb78921
Chris-Duignan <cduignan.1993@gmail.com>
2022-11-03 11:36:27 +0000

    add category filter to sort form

--- src/components/SortForm.jsx
+++ src/components/SortForm.jsx
@@ -1,14 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { fetchCategories } from "../api";
 
 const SortForm = ({ setSearchParams }) => {
   const sortTerms = ["created_at", "comment_count", "votes"];
   const order = ["asc", "desc"];
 
+  const [categories, setCategories] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
   const [searchTerm, setSearchTerm] = useState({
+    category: "strategy",
     sort_by: "created_at",
     order: "desc",
   });
 
+  useEffect(() => {
+    setIsLoading(true);
+    fetchCategories().then((categories) => {
+      setCategories(categories);
+      setIsLoading(false);
+    });
+  }, []);
+
+  console.log(searchTerm);
   const handleChange = (event) => {
     setSearchTerm({ ...searchTerm, [event.target.name]: event.target.value });
   };
@@ -18,8 +31,24 @@ const SortForm = ({ setSearchParams }) => {
     setSearchParams(searchTerm);
   };
 
+  if (isLoading) return <h3>Loading ...</h3>;
   return (
     <form onSubmit={handleSubmit} className="sortForm">
+      <label>
+        Category:
+        <select
+          name="category"
+          id="category"
+          onChange={handleChange}
+          value={searchTerm.category}
+        >
+          {categories.map((category) => (
+            <option key={category.slug} name="category" value={category.slug}>
+              {category.slug}
+            </option>
+          ))}
+        </select>
+      </label>
       <label>
         Sort By:
         <select
