faacbedbe4b2f8d85d2e6a81822cb61ff8818d96
Chris-Duignan <cduignan.1993@gmail.com>
2022-11-06 09:26:39 +0000

    add category cards

---
+++ src/components/CategoryCard.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+const CategoryCard = (data) => {
+  return (
+    <Link className="link cardLink" to={`/categories/${data.slug}`} key={data.slug}>
+      <li key={data.slug} className="reviewCard card">
+        <h4>{data.slug}</h4>
+        <p>{data.description}</p>
+      </li>
+    </Link>
+  );
+};
+
+export default CategoryCard;
