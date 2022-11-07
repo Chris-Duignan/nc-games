948ff2f4c642b4727cc1c0f0c315eceab8ebc327
Chris-Duignan <77028525+Chris-Duignan@users.noreply.github.com>
2022-11-07 09:58:14 +0000

    Revert "7 refactor votes"

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -2,13 +2,9 @@ import { Link } from "react-router-dom";
 
 const CategoryCard = (data) => {
   return (
-    <Link
-      className="link cardLink"
-      to={`/categories/${data.slug}`}
-      key={data.slug}
-    >
-      <li key={data.slug} className="card">
-        <h4>{data.slug}: </h4>
+    <Link className="link cardLink" to={`/categories/${data.slug}`} key={data.slug}>
+      <li key={data.slug} className="reviewCard card">
+        <h4>{data.slug}</h4>
         <p>{data.description}</p>
       </li>
     </Link>
